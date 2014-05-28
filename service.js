0e7b9c0730341a17d7eb68269131d3103fe4b180
Theo Julienne <theo@ninjablocks.com>
2014-05-28 16:16:59 +1000

    Add extremely early bugsnag hook.

--- service.js
+++ service.js
@@ -1,5 +1,10 @@
 process.title = 'sphere-activation-service';
 
+if (process.env.BUGSNAG_KEY) {
+  var bugsnag = require("bugsnag");
+  bugsnag.register(process.env.BUGSNAG_KEY, { releaseStage: process.env.USVC_CONFIG_ENV || 'development' });
+}
+
 var usvc = require('usvc');
 
 var service = usvc.microService({
