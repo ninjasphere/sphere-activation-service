dc4c482dc9e2e9dfa29d36ee9dbd761002632b46
Theo Julienne <theo@ninjablocks.com>
2014-05-30 12:42:27 +1000

    Update schema to include new id column.

---
+++ README.md
@@ -0,0 +1,12 @@
+Activation Service
+=============
+
+This service manages nodes and their tokens, and is the companion of mqtt-proxy.
+
+Provides an internal RPC for activating nodes and their tokens.
+
+Database schema can be set up using:
+
+```
+  mysql -u... < mysql/table_schema.sql
+```
--- mysql/table_schema.sql
+++ mysql/table_schema.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS `nodes` (
+  `id` int(20) NOT NULL AUTO_INCREMENT UNIQUE,
   `user_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `node_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `site_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
@@ -23,3 +24,6 @@ CREATE TABLE IF NOT EXISTS `sites` (
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`, `site_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- alter table nodes add column mqtt_client_id int(20) not null auto_increment unique first;
